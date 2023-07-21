ad2d29c7fcd79be9ef7cad266664510d3baad534
ねじくぎ <63852665+scrwnl@users.noreply.github.com>
2023-07-21 15:31:01 +0000

    Update test

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,5 +1,14 @@
 from watsonpy import parse
 
-def test_parse_Inew_01():
-    watson_code = "B"
-    assert parse(watson_code) == [0]
+
+def test_parse_integer_01():
+    watson_code = "BBuaBubaBubbbaBubbbbaBubbbbbaBubbbbbba"
+    assert parse(watson_code) == [123]
+
+def test_parse_string_01():
+    watson_code = "?SShaakShaaaakShaaaaakShaaaaaak-SShkShaaaaakShaaaaaak-SShkShakShaaakShaaaaakShaaaaaak-SShkShakShaakShaaakShaaaaakShaaaaaak-"
+    assert parse(watson_code) == ["tako"]
+
+def test_parse_object_01():
+    watson_code = """~?ShaaaaaarrShaaaaarrkShaaarrk-SameeShaaaaaarrShaaaaarrkShaarrkShrrk-ShaaaaaarrShaaaaakSameeShaaarrkShaarrk-ShaaaaaarrShaaaaarrkShaaarrkShaarrk-ShaaaaaarrShaaaaarrkShaaarrkShaarrkSharrkShrrk-$BubbbbbbBubbbbbaBubbbbaBubbaBubaBua!BubbbbbbBubbbbbaBubbbaBubbaBubaBua!BubbbbbbBubbbbbaBubbbbaBuba!BubbbbbbBubbbbbaBubbbaBubba!BubbbbbbBubbbbbaBubba!M?ShaaaaaaShaaaaakShaakShak-ShaaaaaaShaaaaakShaaakShk-ShaaaaaaShaaaaakShaaaakShak-ShaaaaaaShaaaaakShaaaakShakShk-ShaaaaaaShaaaaakShaaaakShaak-^!!!!!!!!!!!!!g"""
+    assert parse(watson_code) == [{"first":True,"hello":"world"}]
