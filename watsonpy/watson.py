de562299db95e3fd98e11c3bca27ba2ff34db0cc
ねじくぎ <63852665+scrwnl@users.noreply.github.com>
2023-07-21 15:01:57 +0000

    Update watson.py

--- watsonpy/watson.py
+++ watsonpy/watson.py
@@ -2,11 +2,28 @@ from collections import deque
 import numpy as np
 
 class WatsonVmError(Exception):
-    pass
+    """
+    Exception class for WATSON VM errors
+    """
 
-def parse(code):
+
+
+def parse(code:str):
+    """
+    Parse a string of WATSON code and return a list
+
+    Parameters
+    ----------
+    code : str
+        The string of WATSON code.
+
+    Returns
+    -------
+    list
+        The result of parsing
+    """
+
     # WATSON Instructions
-
     INEW=['B','S']
     IINC=['u','h']
     ISHL=['b','a']
@@ -40,59 +57,87 @@ def parse(code):
             stack.append(0)
 
         elif code[codeptr] == IINC[state]:
-            x = stack.pop()
+            try:
+                x = stack.pop()
+            except IndexError as exc:
+                raise WatsonVmError("Syntax Error") from exc
+
             if type(x) is int:
                 x = np.int64(x)
                 stack.append((x+1).item())
             else:
-                raise WatsonVmError()
-            
+                raise WatsonVmError("Syntax Error")
+
         elif code[codeptr] == ISHL[state]:
-            x = stack.pop()
+            try:
+                x = stack.pop()
+            except IndexError as exc:
+                raise WatsonVmError("Syntax Error") from exc
+
             if type(x) is int:
                 x = np.int64(x)
                 stack.append((x<<1).item())
             else:
-                raise WatsonVmError()
-            
+                raise WatsonVmError("Syntax Error")
+
         elif code[codeptr] == IADD[state]:
-            y = stack.pop()
-            x = stack.pop()
+            try:
+                y = stack.pop()
+                x = stack.pop()
+            except IndexError as exc:
+                raise WatsonVmError("Syntax Error") from exc
+
             if type(x) is int and type(y) is int:
                 y = np.int64(y)
                 x = np.int64(x)
                 stack.append((x+y).item())
             else:
-                raise WatsonVmError()
-            
+                raise WatsonVmError("Syntax Error")
+
         elif code[codeptr] == INEG[state]:
-            x = stack.pop()
+            try:
+                x = stack.pop()
+            except IndexError as exc:
+                raise WatsonVmError("Syntax Error") from exc
+
             if type(x) is int:
                 x = np.int64(x)
                 stack.append((-x).item())
             else:
-                raise WatsonVmError()
-            
+                raise WatsonVmError("Syntax Error")
+
         elif code[codeptr] == ISHT[state]:
-            y = stack.pop()
-            x = stack.pop()
+            try:
+                y = stack.pop()
+                x = stack.pop()
+            except IndexError as exc:
+                raise WatsonVmError("Syntax Error") from exc
+
             if type(y) is int and type(x) is int:
                 y = np.int64(y)
                 x = np.int64(x)
                 stack.append((x << y).item())
             else:
-                raise WatsonVmError()
-        
+                raise WatsonVmError("Syntax Error")
+
         elif code[codeptr] == ITOF[state]:
-            x = stack.pop()
+            try:
+                x = stack.pop()
+            except IndexError as exc:
+                raise WatsonVmError("Syntax Error") from exc
+
             if type(x) is int:
                 x = np.int64(x)
                 stack.append(x.astype(np.float32).item())
             else:
-                raise WatsonVmError()
-            
+                raise WatsonVmError("Syntax Error")
+
         elif code[codeptr] == ITOU[state]:
-            x = stack.pop()
+            try:
+                x = stack.pop()
+            except IndexError as exc:
+                raise WatsonVmError("Syntax Error") from exc
+
             if type(x) is int:
                 x = np.int64(x)
                 stack.append(x.astype(np.uint64).item())
@@ -104,12 +149,16 @@ def parse(code):
             stack.append(float("nan"))
 
         elif code[codeptr] == FNEG[state]:
-            x = stack.pop()
+            try:
+                x = stack.pop()
+            except IndexError as exc:
+                raise WatsonVmError("Syntax Error") from exc
+
             if type(x) is float:
                 stack.append(-x)
 
             else:
-                raise WatsonVmError()
+                raise WatsonVmError("Syntax Error")
 
         elif code[codeptr] == SNEW[state]:
             stack.append("")
@@ -119,8 +168,12 @@ def parse(code):
                 state = 0
 
         elif code[codeptr] == SADD[state]:
-            x = stack.pop()
-            s = stack.pop()
+            try:
+                x = stack.pop()
+                s = stack.pop()
+            except IndexError as exc:
+                raise WatsonVmError("Syntax Error") from exc
+
             if type(x) is int and type(s) is str:
                 s += chr(x)
                 stack.append(s)
@@ -129,9 +182,13 @@ def parse(code):
             stack.append({})
 
         elif code[codeptr] == OADD[state]:
-            v = stack.pop()
-            k = stack.pop
-            o = stack.pop()
+            try:
+                v = stack.pop()
+                k = stack.pop
+                o = stack.pop()
+            except IndexError as exc:
+                raise WatsonVmError("Syntax Error") from exc
+
             if type(k) is str and type(o) is dict:
                 o[k] = v
                 stack.append(o)
@@ -140,8 +197,12 @@ def parse(code):
             stack.append([])
 
         elif code[codeptr] == AADD[state]:
-            x = stack.pop()
-            a = stack.pop()
+            try:
+                x = stack.pop()
+                a = stack.pop()
+            except IndexError as exc:
+                raise WatsonVmError("Syntax Error") from exc
+
             if type(x) is int and type(a) is list:
                 a.append(x)
                 stack.append(a)
@@ -150,7 +211,11 @@ def parse(code):
             stack.append(False)
 
         elif code[codeptr] == BNEG[state]:
-            x = stack.pop()
+            try:
+                x = stack.pop()
+            except IndexError as exc:
+                raise WatsonVmError("Syntax Error") from exc
+
             if type(x) is bool:
                 stack.append(not x)
 
@@ -158,20 +223,29 @@ def parse(code):
             stack.append(None)
 
         elif code[codeptr] == GDUP[state]:
-            x = stack.pop()
+            try:
+                x = stack.pop()
+            except IndexError as exc:
+                raise WatsonVmError("Syntax Error") from exc
+
             stack.append(x)
             stack.append(x)
 
         elif code[codeptr] == GPOP[state]:
-            stack.pop()
+            try:
+                stack.pop()
+            except IndexError as exc:
+                raise WatsonVmError("Syntax Error") from exc
 
         elif code[codeptr] == GSWP[state]:
-            if len(stack) >= 2:
+            try:
                 y = stack.pop()
                 x = stack.pop()
-                stack.append(y)
-                stack.append(x)
-            else:
-                raise WatsonVmError()
-            
+            except IndexError as exc:
+                raise WatsonVmError("Syntax Error") from exc
+
+            stack.append(y)
+            stack.append(x)
+
+        codeptr += 1
     return list(stack)
